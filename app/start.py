8cd03391a07cdca20d2875144090e76c282b12af
ovr4ulin <82352000+ovr4ulin@users.noreply.github.com>
2021-11-09 12:56:23 -0300

    Update start.py

--- app/start.py
+++ app/start.py
@@ -4,13 +4,13 @@ import time
 flash = machine.Pin(4, machine.Pin.OUT)
 
 while True:
-    print("Hola Mati")
+    print("Hola Maxi")
     flash.off()
     time.sleep(0.5)
     flash.on()
     time.sleep(0.5)
     flash.off()
-    print("Chau Mati")
+    print("Chau Maxi")
     time.sleep(2)
     flash.on()
     time.sleep(2)
