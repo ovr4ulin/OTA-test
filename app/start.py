d3d5c765991cbb44fff5b59c46cacafe297f76e0
ovr4ulin <82352000+ovr4ulin@users.noreply.github.com>
2021-11-09 10:39:05 -0300

    modify time sleep

--- app/start.py
+++ app/start.py
@@ -1,9 +1,10 @@
 import machine
 import time
 
-flash = machine.Pin(14, machine.Pin.OUT)
+flash = machine.Pin(4, machine.Pin.OUT)
 
 while True:
     flash.off()
     time.sleep(1)
     flash.on()
+    time.sleep(1)
